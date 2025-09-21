e64bfb3db802be64c4e84748fdb9591b546f7407
郝某人BH <1218271192@qq.com>
2025-09-21 18:53:24 +0800

    Update get_resource.py

--- amcl/src/amcl/utils/get_resource.py
+++ amcl/src/amcl/utils/get_resource.py
@@ -24,7 +24,7 @@ from pathlib import Path
 def r(relative_path):
     # the packaged environment
     if getattr(sys, 'frozen', False):
-        base_path = Path(sys.executable).parent.parent
+        base_path = Path(sys.executable)
     # the development environment
     else:
         base_path = Path(__file__).resolve().parent.parent.parent.parent
