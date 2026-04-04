3f4309b4b0f9b7362184e8440da9c11acfffb8fd
郝某人BH <1218271192@qq.com>
2026-04-04 16:22:47 +0800

    添加 Rust 项目结构和 UI 组件

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::error::Error;
+
+use slint::LogicalPosition;
+
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+
+    let ui_weak = ui.as_weak();
+
+    ui.on_move_window(move |x, y| {
+        if let Some(ui) = ui_weak.upgrade() {
+            let window = ui.window();
+            let logical_pos = window.position().to_logical(window.scale_factor());
+            window.set_position(LogicalPosition::new(
+                logical_pos.x + x,
+                logical_pos.y + y,
+            ));
+        }
+    });
+
+    ui.on_close_window(|| {
+        std::process::exit(0);
+    });
+
+    ui.run()?;
+
+    Ok(())
+}
